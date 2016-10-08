18cc98a8e7dedc36c2b75f70a76325ac3a864f1a
isalfati <ivansalfatift@gmail.com>
2016-10-08 15:11:53 +0200

    submit

--- Server/router.js
+++ Server/router.js
@@ -5,14 +5,22 @@ router.get("/", function(req, res){
 	res.sendFile( __dirname + "/" + "map_1.html");
 });
 
-router.get('/process_get', function(req, res){
+router.get('/get_date_loc', function(req, res){
 	// Preprare output in JSON format
 	response = {
 		date:req.query.date,
 		localization:req.query.localization
 	};
+	// Esta response se enviará a a la query
 	console.log(response);
 	res.end(JSON.stringify(response));
 });
 
+router.get('/get_num', function(req,res){
+	response = {
+		num:req.query.num
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+})
 module.exports = router;
