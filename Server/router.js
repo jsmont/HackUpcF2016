1818e40f6d5792d8b63ecd2f6a9b1d8756e6f69b
isalfati <ivansalfatift@gmail.com>
2016-10-08 12:40:29 +0200

    Pick date, location

--- Server/router.js
+++ Server/router.js
@@ -5,4 +5,14 @@ router.get("/", function(req, res){
 	res.sendFile( __dirname + "/" + "map_1.html");
 });
 
+router.get('/process_get', function(req, res){
+	// Preprare output in JSON format
+	response = {
+		date:req.query.date,
+		localization:req.query.localization
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+});
+
 module.exports = router;
