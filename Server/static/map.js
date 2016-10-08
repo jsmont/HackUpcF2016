0d7c67a9e08ea89a3867281e0cc78d91597c283a
Sola <jordi.sola@hp.com>
2016-10-08 10:09:59 +0200

    asd

--- Server/static/map.js
+++ Server/static/map.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY = "AIzaSyAQwGT6uVFpwLcyJ1kmJFJzx6rL5uUMmYY"
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 41.3947687, lng: 2.0785561},
+    zoom: 12
   });
 };
